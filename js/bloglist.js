2bab77917184dda3e002216cb8b2af33567859fe
NIKHIL ARORA <nikhilarora@NIKHILs-MacBook-Pro.local>
2019-08-11 21:21:42 +0530

    Some modifications in modal and part B completed.

---
+++ js/bloglist.js
@@ -0,0 +1,17 @@
+$(".more").click(function() {
+  window.location.href = "../html/post.html";
+});
+
+function nextPage(){
+    window.location="../html/post.html";
+}
+
+// function to display Delete modal on trash icon click
+function displayModal() {
+  deleteModal.style.display = "block";
+}
+
+//function to display bloglist.html if no is selected in the delete modal
+function dModal(){
+    deleteModal.style.display = "none";
+}
---
+++ js/index.js
@@ -0,0 +1,11 @@
+signInMethod();
+{
+  //Left for future implementation
+  //code for taking fields from html goes here.
+}
+
+signUpMethod();
+{
+  //left for future implementation
+  //code for taking feilds from html goes here.
+}
